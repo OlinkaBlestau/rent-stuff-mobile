9fdfdad2b3fec80638052ffa1aae18f8df1a1fe4
Artem Kuptsov <kuptsov210@gmail.com>
2022-07-03 02:53:11 +0300

    sorting and filtration

--- app/src/main/java/alina/zahovora/nure/fragment/AnnouncementFragment.kt
+++ app/src/main/java/alina/zahovora/nure/fragment/AnnouncementFragment.kt
@@ -9,12 +9,17 @@ import alina.zahovora.nure.R
 import alina.zahovora.nure.adapter.AnnouncementsListAdapter
 import alina.zahovora.nure.api.objects.GetAllAnnouncements
 import alina.zahovora.nure.data.Announcement
+import android.widget.AdapterView
 import android.widget.ListView
+import android.widget.Spinner
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 class AnnouncementFragment : Fragment() {
+    private var sort: Spinner? = null
+    private var filter: Spinner? = null
+    private var rootView: View? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,9 +29,45 @@ class AnnouncementFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val view = inflater.inflate(R.layout.fragment_announcements_list, container, false)
-        getAnnouncements(view)
-        return view
+        rootView = inflater.inflate(R.layout.fragment_announcements_list, container, false)
+
+        sort = rootView?.findViewById(R.id.spinnerSorting)
+        filter = rootView?.findViewById(R.id.spinnerSpecies)
+
+        sort?.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                getAnnouncements(rootView!!)
+            }
+
+        }
+
+        filter?.onItemSelectedListener = object : AdapterView.OnItemSelectedListener {
+            override fun onNothingSelected(parent: AdapterView<*>?) {
+
+            }
+
+            override fun onItemSelected(
+                parent: AdapterView<*>?,
+                view: View?,
+                position: Int,
+                id: Long
+            ) {
+                getAnnouncements(rootView!!)
+            }
+
+        }
+
+        getAnnouncements(rootView!!)
+        return rootView
     }
 
     private fun getAnnouncements(view: View) {
@@ -44,8 +85,76 @@ class AnnouncementFragment : Fragment() {
                 println(response.code())
                 println(response.body())
 
+                val sortItem = sort?.selectedItem.toString()
+                val filterItem = filter?.selectedItem.toString()
+                var announcements: ArrayList<Announcement> = response.body()!!
+
+                announcements = when (sortItem) {
+                    "Вартність ↑" -> {
+                        announcements.sortedBy { it.price }.toCollection(ArrayList<Announcement>())
+                    }
+                    "Вартність ↓" -> {
+                        announcements.sortedByDescending { it.price }
+                            .toCollection(ArrayList<Announcement>())
+                    }
+                    else -> {
+                        announcements
+                    }
+                }
+
+                announcements = when (filterItem) {
+                    "Техніка для дома" -> {
+                        announcements.filter { announcement ->
+                            announcement.category?.name.equals("Техніка для дома")
+                        }.toCollection(ArrayList<Announcement>())
+                    }
+                    "Фото та відео" -> {
+                        announcements.filter { announcement ->
+                            announcement.category?.name.equals("Фото та відео")
+                        }.toCollection(ArrayList<Announcement>())
+                    }
+                    "Інструменти" -> {
+                        announcements.filter { announcement ->
+                            announcement.category?.name.equals("Інструменти")
+                        }.toCollection(ArrayList<Announcement>())
+                    }
+                    "Музичні інструменти" -> {
+                        announcements.filter { announcement ->
+                            announcement.category?.name.equals("Музичні інструменти")
+                        }.toCollection(ArrayList<Announcement>())
+                    }
+                    "Спортивні тренажери" -> {
+                        announcements.filter { announcement ->
+                            announcement.category?.name.equals("Спортивні тренажери")
+                        }.toCollection(ArrayList<Announcement>())
+                    }
+                    "Спорт та відпочинок" -> {
+                        announcements.filter { announcement ->
+                            announcement.category?.name.equals("Спорт та відпочинок")
+                        }.toCollection(ArrayList<Announcement>())
+                    }
+                    "Устаткування для заходів" -> {
+                        announcements.filter { announcement ->
+                            announcement.category?.name.equals("Устаткування для заходів")
+                        }.toCollection(ArrayList<Announcement>())
+                    }
+                    "Електроніка" -> {
+                        announcements.filter { announcement ->
+                            announcement.category?.name.equals("Електроніка")
+                        }.toCollection(ArrayList<Announcement>())
+                    }
+                    "Ігри та консолі" -> {
+                        announcements.filter { announcement ->
+                            announcement.category?.name.equals("Ігри та консолі")
+                        }.toCollection(ArrayList<Announcement>())
+                    }
+                    else -> {
+                        announcements
+                    }
+                }
+
                 val listView = view.findViewById<ListView>(R.id.announcements_list)
-                val adapter = AnnouncementsListAdapter(view.context, response.body()!!)
+                val adapter = AnnouncementsListAdapter(view.context, announcements)
                 listView.adapter = adapter
             }
         })
